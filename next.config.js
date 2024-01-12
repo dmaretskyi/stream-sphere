5294b24f5994dc7c601550c12f1743a0eddf86d8
Dmytro Maretskyi <maretskii@gmail.com>
2024-01-12 23:57:55 +0100

    Add static search results

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+     // TODO(dmaretskyi): proxy images.
+     remotePatterns: [{
+      protocol: 'https',
+      hostname: '**',
+      port: '',
+    }]
+  }
+}
 
 module.exports = nextConfig
